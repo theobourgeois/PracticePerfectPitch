7c0531bc6e7fdd02004c4227a316bcc11473c96f
theobourgeois <theoobourgeois@gmail.com>
2024-03-02 00:57:42 -0400

    chnage title

--- src/components/quiz/quiz.tsx
+++ src/components/quiz/quiz.tsx
@@ -11,13 +11,12 @@ export function Quiz() {
         for (const note of guessNotes) {
             play(note);
         }
-        console.log(guessNotes);
     }, [guessNotes]);
     return (
         <div className="flex h-full gap-4 flex-col justify-center items-center">
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 mb-4">
                 <h1 className="text-center text-6xl font-bold">
-                    Train your pitch
+                    Practice Your Pitch
                 </h1>
                 <h2 className="text-center text-3xl">Guess the note</h2>
             </div>
